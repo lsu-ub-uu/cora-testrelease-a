a6142110c02faf41e52ad9fc04abd35cf6a4f3f7
olovm <olov.mckie@ub.uu.se>
2019-09-20 12:04:46 +0000

    inital commit

---
+++ src/main/java/se/uu/ub/cora/testrelease/App.java
@@ -0,0 +1,13 @@
+package se.uu.ub.cora.testrelease;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
